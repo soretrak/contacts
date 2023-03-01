457294c06cbe5d33b0d01b46f396b26fef010c7a
fakhrikharrat <fakhri.unitedwings@gmail.com>
2023-03-01 12:41:03 +0100

    modif

--- contacts-frontend/src/environments/environment.prod.ts
+++ contacts-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  contactAPI: 'http://35.222.131.185/backend/api/contacts'
+  // contactAPI: 'http://35.222.131.185/backend/api/contacts'
+  contactAPI: 'http://10.1.1.245:30500/backend/api/contacts'
+
 };
--- contacts-frontend/src/environments/environment.ts
+++ contacts-frontend/src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  contactAPI: 'http://35.222.131.185/backend/api/contacts'
+  // contactAPI: 'http://35.222.131.185/backend/api/contacts'
+  contactAPI: 'http://10.1.1.245:30500/backend/api/contacts'
+  
 };
 
 /*
